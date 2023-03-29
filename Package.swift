246538f61d4eafe742c9c132ffe1e710b4cabb8b
Emil Aleborn <emil.aleborn@mobileinteraction.se>
2023-03-29 15:10:30 +0200

    Update maven repo url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerfoyleRealm",
-            url: "http://51.12.242.35:8081/repository/maven-releases/com/exeger/powerfoyle/realm-xcframework/1.2.2/realm-xcframework-1.2.2.zip",
+            url: "https://maven.exeger.com/repository/maven-releases/com/exeger/powerfoyle/realm-xcframework/1.2.2/realm-xcframework-1.2.2.zip",
             checksum: "eff1b8c55fafbeb8366732a9ddf9cec59f6fdf4c302fc55b15e40796951f62e8"
         ),
     ]
