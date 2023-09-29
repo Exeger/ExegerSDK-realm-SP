492f43ce6b5be7681b0f817102f1c7b08552ae4f
Erik H <erik.hedrenius@mobileinteraction.se>
2023-09-29 13:28:16 +0200

    Update checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "PowerfoyleRealm",
             url: "https://pkgs.dev.azure.com/exeger/PowerfoyleSDK/_packaging/repository/maven/v1/com.exeger.powerfoyle/realm-xcframework/1.2.4/realm-xcframework-1.2.4.zip",
-            checksum: "e2797811964681675ba301426d4a2e0c4353f14a453a4e5dcd655a6896f964f0"
+            checksum: "c1626c039bc0fa834bc2154e7c2d3c2153cf0e330808d0415c9e69d2e39b512c"
         ),
     ]
 )
